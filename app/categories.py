833ba9746784ae74b8e8b850d39c2b5574766ac1
negativenagesh <gaonkarsub@gmail.com>
2026-03-19 21:21:53 +0530

    issue categories

---
+++ app/categories.py
@@ -0,0 +1,9 @@
+ISSUE_CATEGORIES = [
+    "order_tracking",
+    "delivery_delays",
+    "refund_requests",
+    "product_complaints",
+    "subscription_issues",
+    "payment_failures",
+    "general_product_questions",
+]
